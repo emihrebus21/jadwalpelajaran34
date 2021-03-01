13331067822c60aff3ead67a65c138c7730bf4bc
emihrebus21 <50658342+emihrebus21@users.noreply.github.com>
2021-03-01 19:29:41 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+print('Jadwal Pelajaran Kelas 9 SMP 34 Bandung')
+tanya = input("Hari / pelajaran: ")
+if tanya == "senin":
+    print(" Jadwal hari senin: \n | 7: 30 - 09: 00  PKN |\n | 09: 30 - 10: 00 IPS | ")
+if tanya == "selasa":
+    print(" Jadwal hari selasa: \n | 7: 30 - 09: 00  Indonesia |\n | 09: 30 - 10: 00 Inggris | ")
+if tanya == "rabu":
+    print(" Jadwal hari rabu: \n | 7: 00 - 08: 30  IPA |\n | 09: 00 - 10: 30 MTK |\n | 10.30 - 12.00 PJOK | ")
+if tanya == "kamis":
+    print(" Jadwal hari kamis: \n | 7: 30 - 09: 00  Sunda |\n | 09: 30 - 11: 00 TIK | ")
+if tanya == "jumat":
+    print(" Jadwal hari jumat: \n | 7: 30 - 09: 00  PAI |\n | 09: 30 - 11: 00 Senbud | ")
+if tanya == "pkn":
+    print("|Senin 7:30 - 09:00 PKN |")
+if tanya == "ips":
+    print("|Senin 09:30 - 10:00 IPS |")
+if tanya == "indo":
+    print("|Selasa 7: 30 - 09: 00  Indonesia |")
+if tanya == "inggris":
+    print("|Selasa 09: 30 - 10: 00 Inggris |")
+if tanya == "ipa":
+    print("|Rabu 7: 00 - 08: 30  IPA |")
+if tanya == "mtk":
+    print("|Rabu 09: 00 - 10: 30 MTK |")
+if tanya == "pjok":
+    print("|Rabu 10.30 - 12.00 PJOK |")
+if tanya == "sunda":
+    print("|Kamis 7: 30 - 09: 00  Sunda |")
+if tanya == "tik":
+    print("|Kamis 09: 30 - 11: 00 TIK |")
+if tanya == "PAI":
+    print("|Jumat 7: 30 - 09: 00  PAI |")
+if tanya == "senbud":
+    print("|Jumat 09: 30 - 11: 00 Senbud |")
+if tanya == "bk":
+    print("|Rabu 11: 00 - 12: 00  BK |")
